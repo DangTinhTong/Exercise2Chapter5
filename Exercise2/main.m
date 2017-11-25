74ef841380b8993097af0d460fc14afef92c4b87
admin <dangtinhtong@gmail.com>
2017-11-25 23:23:54 +0700

    first commit

--- Exercise2/main.m
+++ Exercise2/main.m
@@ -11,7 +11,16 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+      //NSLog(@"Hello, World!");
+        
+        int n;
+        double triangular;
+        triangular=0;
+        for(n=0;n<10;++n)
+        {
+            triangular=n*(n+1)/2;
+        NSLog(@"%i  %g",n,triangular);
+        }
     }
     return 0;
 }
